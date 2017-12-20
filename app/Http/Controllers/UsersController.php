e161c7d574e738324e0d4a4f4858394830a3ff7e
Yuan xin <2150919412@qq.com>
2017-12-20 02:27:16 +0000

    Add user statuses

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -28,7 +28,10 @@ class UsersController extends Controller
 
     public function show(User $user)
     {
-        return view('users.show', compact('user'));
+        $statuses = $user->statuses()
+                           ->orderBy('created_at', 'desc')
+                           ->paginate(30);
+        return view('users.show', compact('user', 'statuses'));
     }
 
     //表单数据提交后的 store 方法，用于处理用户创建的相关逻辑。
